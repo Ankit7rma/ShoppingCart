3d27e60999287d413697a4437ed71abc80e0ab40
Ankit Sharma <ankitsharma290101@gmail.com>
2023-10-03 11:13:33 +0530

    If Cart Empty Handled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,16 +12,15 @@ const Cart = () => {
   }
   return (
     <div className='flex flex-wrap justify-evenly'>
-{
+{ !items.length?"Add Items to Cart":
   items.map((p)=>{
     return  <div className='flex flex-col justify-center items-center'> 
      <ProductCard key={p.id} id={p.id} image={p.image} title={p.title} description={p.description} price={p.price}/>
      <button className='bg-red-300 border w-[50%] border-gray-500 rounded-lg p-2 ' onClick={()=>{handleClick(p)}}>
     Remove</button>
      </div>
-  })
-}
-    </div>
+  })}
+     </div>
   )
 }
 
